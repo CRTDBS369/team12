e74276482208b45e0a40aefd5af22f464b3bb72d
Lemondouble <lemondouble2@gmail.com>
2021-06-14 17:29:17 +0900

    add index page and controllers/public/view folders

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require('express')
+const ejs = require('ejs')
 const app = new express()
 const connectDB = require('./DB/Connection');
+const fileUpload = require('express-fileupload')
+
 
 const loginUserController = require('./Api/loginUser')
 
+const homeController = require('./controllers/home')
+const login_registerController = require('./controllers/login_register')
+
+app.set('view engine','ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+app.use(fileUpload())
+
+
 connectDB();
 
-app.listen(4000, ()=>{
+app.get('/', homeController)
+app.get('/login_register', login_registerController)
+
+
+app.listen(50005, ()=>{
     console.log('start')
 })
